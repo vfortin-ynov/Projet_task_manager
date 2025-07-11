78eca4ac60a2509cd1c9d6e2a6cd93f93f73eeb3
vfortin-ynov <valentin.fortin@ynov.com>
2025-07-11 11:35:08 +0200

    test: add pytest configuration and fixtures auto-discovery

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+"""Configuration file for pytest."""
+# This file ensures that fixtures are automatically discovered by pytest
+from tests.fixtures.manager_fixtures import *
+from tests.fixtures.task_fixtures import *
